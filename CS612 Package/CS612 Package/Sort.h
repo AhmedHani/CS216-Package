62cfff38d8d8c96e98bbe878426b4125883f174b
Ahmed Hani Ibrahim <ahani.fcis@gmail.com>
2016-03-26 22:36:19 +0200

    Selection and Insertion sorting added.

--- CS612 Package/CS612 Package/Sort.h
+++ CS612 Package/CS612 Package/Sort.h
@@ -27,6 +27,46 @@ public:
 
 		return sorted_list;
 	}
+
+	T* selection_sort() {
+		T* sorted_list = this->data;
+
+		for (size_t i = 0; i < this->data_size - 1; i++) {
+			int current_min_index = i;
+
+			for (size_t j = i + 1; j < this->data_size; j++) {
+				if (sorted_list[j] < sorted_list[current_min_index]) {
+					current_min_index = j;
+				}
+			}
+
+			if (current_min_index != i) {
+				T temp = sorted_list[i];
+				sorted_list[i] = sorted_list[current_min_index];
+				sorted_list[current_min_index] = temp;
+			}
+		}
+
+		return sorted_list;
+	}
+
+	T* insertion_sort() {
+		T* sorted_list = this->data;
+
+		for (int i = 1; i < this->data_size; i++) {
+			T current = sorted_list[i];
+			int prev = i - 1;
+
+			while (prev >= 0 && sorted_list[prev] > current) {
+				sorted_list[prev + 1] = sorted_list[prev];
+				prev -= 1;
+			}
+
+			sorted_list[prev + 1] = current;
+		}
+
+		return sorted_list;
+	}
 };
 
 
--- CS612 Package/CS612 Package/Source.cpp
+++ CS612 Package/CS612 Package/Source.cpp
@@ -4,5 +4,19 @@
 using namespace std;
 
 int main() {
+	int* arr = new int[3];
+	arr[1] = 3;
+	arr[0] = 6;
+	arr[2] = 5;
+
+	int length = 3;
+
+	Sort<int> s(arr, length);
+	arr = s.insertion_sort();
+
+	for (int i = 0; i < length; i++) {
+		cout << arr[i] << endl;
+	}
+
 	return 0;
 }
